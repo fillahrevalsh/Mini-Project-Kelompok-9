997e68352ba52b335dd8dd2323119b73a510d32f
FarrelJonata <farreljonata123@gmail.com>
2025-11-08 18:48:35 +0700

    tabungan.cpp

---
+++ tabungan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void tampilkanMenu() {
+    cout << "================================" << endl;
+    cout << "     PROGRAM TABUNGAN HARIAN    " << endl;
+    cout << "================================" << endl;
+    cout << "1. Setor Uang" << endl;
+    cout << "2. Tarik Uang" << endl;
+    cout << "3. Total & Rata-rata Saldo" << endl;
+    cout << "4. Tabungan Terbesar & Terkecil" << endl;
+    cout << "5. Keluar" << endl;
+    cout << "================================" << endl;
+}
